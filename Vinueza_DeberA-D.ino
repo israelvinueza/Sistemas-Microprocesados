3540cd7aa2994f993ce56fe1878cc12543de36dd
israelvinueza <57198886+israelvinueza@users.noreply.github.com>
2019-11-29 20:37:10 -0500

    Add files via upload

---
+++ Vinueza_DeberA-D.ino
@@ -0,0 +1,31 @@
+    //UNIVERSIDAD TÉCNICA DEL NORTE
+              //FICA
+           //CIERCOM
+    //SISTEMAS MICROPROCESADOS
+//Israel Vinueza
+int dato;
+int lectura;
+#define led A3
+int ult;
+void setup() {
+Serial.begin(9600);
+pinMode(led,OUTPUT);
+}
+void loop() {
+ if( Serial.available()>0)
+  {
+    lectura=Serial.readString().toInt();
+    Serial.print(lectura);
+  }
+ dato = analogRead(0);
+ Serial.println(String(analogRead(0)));
+ delay(200);
+ 
+if(ult>dato){
+  digitalWrite(led, HIGH);
+   Serial.println("Mayor valor del pin Análogo Digital");
+   delay(100);
+}else{
+  digitalWrite(led, HIGH);  
+}
+}
